8fc0b2fee8ce985e3bf9641db6f6e9f1d02847e4
y-hrubskyi <y.hrubskyi@gmail.com>
2023-08-25 19:42:07 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+// libraries
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
+
+const formRef = document.querySelector('.feedback-form');
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onFormInput, 500));
+
+const formData = {};
+const parsedData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+for (key in parsedData) {
+  formData[key] = parsedData[key];
+  formRef.elements[key].value = formData[key];
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  console.log(formData);
+  event.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM_KEY);
+}
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(formData));
+}
