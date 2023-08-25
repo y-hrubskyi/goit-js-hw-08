e2f8ad1af0c69a751ff348bbe234e896e54a7997
y-hrubskyi <y.hrubskyi@gmail.com>
2023-08-25 15:36:56 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// libraries
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+// data
+import { galleryItems } from './gallery-items';
+
+// code
+const galleryRef = document.querySelector('.gallery');
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+          </a>
+        </li>`
+    )
+    .join('');
+}
