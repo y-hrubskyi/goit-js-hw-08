235807ee242ad2640c288618a235baf93e393a8d
y-hrubskyi <y.hrubskyi@gmail.com>
2023-08-26 08:33:25 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+// libraries
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIDEOPLAYER_DATA_KEY = 'videoplayer-current-time';
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+
+const seconds = localStorage.getItem(VIDEOPLAYER_DATA_KEY) ?? 0;
+player.setCurrentTime(seconds);
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
+
+function onPlayerTimeUpdate(data) {
+  localStorage.setItem(VIDEOPLAYER_DATA_KEY, data.seconds);
+}
