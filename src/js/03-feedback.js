d3feeb82ad84dc29b25c6d4a60e06cebaff0127d
y-hrubskyi <y.hrubskyi@gmail.com>
2023-08-26 22:55:00 +0300

    Update task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,29 @@
 // libraries
 import throttle from 'lodash.throttle';
 
+// code
 const FEEDBACK_FORM_KEY = 'feedback-form-state';
 
-const refs = {
-  feedbackForm: document.querySelector('.feedback-form'),
-  emailInput: document.querySelector('input[name="email"]'),
-  messageInput: document.querySelector('textarea[name="message"]'),
-};
-
-refs.feedbackForm.addEventListener('submit', onFormSubmit);
-refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+const feedbackFormRef = document.querySelector('.feedback-form');
+feedbackFormRef.addEventListener('submit', onFormSubmit);
+feedbackFormRef.addEventListener('input', throttle(onFormInput, 500));
 
 const parsedData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY)) ?? {};
-const formData = { ...parsedData };
-refs.emailInput.value = formData.email ?? '';
-refs.messageInput.value = formData.message ?? '';
+let formData = { ...parsedData };
+for (const key in formData) {
+  feedbackFormRef.elements[key].value = formData[key];
+}
 
 function onFormSubmit(event) {
   event.preventDefault();
 
+  const { email, message } = event.currentTarget.elements;
+  if (!email.value || !message.value) {
+    return;
+  }
+
   console.log(formData);
+  formData = {};
   event.currentTarget.reset();
   localStorage.removeItem(FEEDBACK_FORM_KEY);
 }
