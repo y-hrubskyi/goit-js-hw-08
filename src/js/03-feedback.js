463e575f232fef3e54b67c9f1bf59a44e5f81165
y-hrubskyi <y.hrubskyi@gmail.com>
2023-08-26 09:12:35 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,15 +3,19 @@ import throttle from 'lodash.throttle';
 
 const FEEDBACK_FORM_KEY = 'feedback-form-state';
 
-const formRef = document.querySelector('.feedback-form');
-formRef.addEventListener('submit', onFormSubmit);
-formRef.addEventListener('input', throttle(onFormInput, 500));
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  emailInput: document.querySelector('input[name="email"]'),
+  messageInput: document.querySelector('textarea[name="message"]'),
+};
 
-const parsedData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY)) || {};
+refs.feedbackForm.addEventListener('submit', onFormSubmit);
+refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+const parsedData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY)) ?? {};
 const formData = { ...parsedData };
-for (key in formData) {
-  formRef.elements[key].value = formData[key];
-}
+refs.emailInput.value = formData.email ?? '';
+refs.messageInput.value = formData.message ?? '';
 
 function onFormSubmit(event) {
   event.preventDefault();
